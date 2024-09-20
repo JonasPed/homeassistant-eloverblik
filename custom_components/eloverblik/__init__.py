dbd9980e8d51d631dd56814c5268b95e90537d97
Jonas Pedersen <jonas@jope.eu>
2024-09-20 21:06:44 +0200

    Correct call to async_forward_entry_setups.

--- custom_components/eloverblik/__init__.py
+++ custom_components/eloverblik/__init__.py
@@ -36,10 +36,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     
     hass.data[DOMAIN][entry.entry_id] = HassEloverblik(refresh_token, metering_point)
 
-    for component in PLATFORMS:
-        await hass.async_create_task(
-            hass.config_entries.async_forward_entry_setups(entry, component)
-        )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
